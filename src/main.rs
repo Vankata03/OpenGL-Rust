9fe05504cc7f6563d0c48dad3a836c13a92dd8ce
Ivan Terziev <ivanterziev38@gmail.com>
2025-05-28 01:06:22 +0300

    Add github workflow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-extern crate sdl2;
 extern crate gl;
+extern crate sdl2;
 
-// Note: Just used for debugging 
+// Note: Just used for debugging
 use std::env;
 
 fn main() {
-
     // Back Trace for debuging
     unsafe {
         env::set_var("RUST_BACKTRACE", "1");
@@ -23,10 +22,12 @@ fn main() {
 
     // Get the event pump
     let mut event_pump = sdl.event_pump().expect("Failed to get event pump");
-    
+
     // Load OpenGL function pointers
-    let _gl_context = window.gl_create_context().expect("Failed to create GL Context");
-    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const _);
+    let _gl_context = window
+        .gl_create_context()
+        .expect("Failed to create GL Context");
+    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const _);
 
     unsafe {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
@@ -34,11 +35,11 @@ fn main() {
 
     // Main program loop
     'main: loop {
-
         // Listen for events/user input
         for event in event_pump.poll_iter() {
             match event {
                 sdl2::event::Event::Quit { .. } => break 'main,
+                sdl2::event::Event::AppLowMemory { .. } => break 'main,
                 _ => {}
             }
         }
