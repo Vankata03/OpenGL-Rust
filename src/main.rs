d7b0296497a0cbc5cb82e3f33bc01f2d25a30252
Ivan Terziev <ivanterziev38@gmail.com>
2025-06-02 00:32:19 +0300

    Build the shaders in main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 extern crate gl;
 extern crate sdl2;
 
-// Note: Just used for debugging
-use std::{env, ffi::CString};
+pub mod render_gl;
 
 fn main() {
-    // Back Trace for debuging
-    unsafe {
-        env::set_var("RUST_BACKTRACE", "1");
-    }
-
     // Initialize sdl and do some setup work
     let sdl = sdl2::init().expect("Failed to initialize SDL2");
     let video_subsystem = sdl.video().expect("Failed to initialize video subsystem");
@@ -40,6 +34,19 @@ fn main() {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
+    use std::ffi::CString;
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
+
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
     // Main program loop
     'main: loop {
         // Listen for events/user input
@@ -57,78 +64,5 @@ fn main() {
         }
 
         window.gl_swap_window();
-    }
-
-    struct Shader {
-        id: gl::types::GLuint,
-    }
-
-    impl Shader {
-        fn from_source(source: &std::ffi::CStr, kind: gl::types::GLenum) -> Result<Shader, String> {
-            let id = shader_from_source(source, kind)?;
-            Ok(Shader { id })
-        }
-
-        fn from_vert_source(source: &std::ffi::CStr) -> Result<Shader, String> {
-            Shader::from_source(source, gl::VERTEX_SHADER)
-        }
-
-        fn from_frag_source(source: &std::ffi::CStr) -> Result<Shader, String> {
-            Shader::from_source(source, gl::FRAGMENT_SHADER)
-        }
-    }
-
-    impl Drop for Shader {
-        fn drop(&mut self) {
-            unsafe {
-                gl::DeleteShader(self.id);
-            }
-        }
-    }
-
-    fn shader_from_source(
-        source: &std::ffi::CStr,
-        kind: gl::types::GLuint,
-    ) -> Result<gl::types::GLuint, String> {
-        let id = unsafe { gl::CreateShader(kind) };
-
-        unsafe {
-            gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-            gl::CompileShader(id)
-        };
-
-        let mut success: gl::types::GLint = 1;
-        unsafe {
-            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-        };
-
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {
-                gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-            }
-
-            let error = create_whitespace_cstring_with_len(len as usize);
-            unsafe {
-                gl::GetShaderInfoLog(
-                    id,
-                    len,
-                    std::ptr::null_mut(),
-                    error.as_ptr() as *mut gl::types::GLchar,
-                )
-            }
-
-            return Err(error.to_string_lossy().into_owned());
-        }
-
-        Ok(id)
-    }
-
-    fn create_whitespace_cstring_with_len(len: usize) -> std::ffi::CString {
-        // Buffer to write the error
-        let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
-        buffer.extend([b' '].iter().cycle().take(len));
-
-        unsafe { std::ffi::CString::from_vec_unchecked(buffer) }
     }
 }
